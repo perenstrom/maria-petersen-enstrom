9223c17f8de4eaed504e28b87bf3a98a39b55e8b
Per Enström <enstrom.per@gmail.com>
2021-08-02 12:22:59 +0200

    Update favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,7 @@ export default class MyDocument extends Document {
 
     ctx.renderPage = () =>
       originalRenderPage({
-        enhanceApp: (App) => (props) =>
-          materialUiSheets.collect(<App {...props} />)
+        enhanceApp: App => props => materialUiSheets.collect(<App {...props} />)
       });
 
     const initialProps = await Document.getInitialProps(ctx);
@@ -34,6 +33,27 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Merienda:400,700&display=swap"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#353535" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
